75a03a3236095290cc1708b59c1268e6ade617d3
riccardo maggi <riccardoma07@gmail.com>
2026-09-22 18:41:55 +0200

    fix(ui): remove version badge from top-left sidebar header, bump to v1.9.6

--- app/version.py
+++ app/version.py
@@ -10,7 +10,7 @@ Convenzione SemVer (MAJOR.MINOR.PATCH):
 - MAJOR: refactoring architetturali o modifiche strutturali/breaking.
 """
 
-__version__ = "1.9.5"
+__version__ = "1.9.6"
 APP_VERSION = __version__
 
 
