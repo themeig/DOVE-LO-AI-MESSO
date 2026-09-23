fdb7494e6b282f60050ae20aabf5946cc1617105
riccardo maggi <riccardoma07@gmail.com>
2026-09-23 21:24:39 +0200

    refactor(v2.2.2): remove redundant Torna alla Chat and Sistema buttons from dashboard header

--- app/version.py
+++ app/version.py
@@ -10,7 +10,7 @@ Convenzione SemVer (MAJOR.MINOR.PATCH):
 - MAJOR: refactoring architetturali o modifiche strutturali/breaking.
 """
 
-__version__ = "2.2.1"
+__version__ = "2.2.2"
 APP_VERSION = __version__
 
 
