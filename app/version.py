e2f73cdee98745e7ac45f73a4c8a7561e22db37c
riccardo maggi <riccardoma07@gmail.com>
2026-09-23 18:09:21 +0200

    fix(v2.1.6): remove bottom chat subtext indicators and bump version

--- app/version.py
+++ app/version.py
@@ -10,7 +10,7 @@ Convenzione SemVer (MAJOR.MINOR.PATCH):
 - MAJOR: refactoring architetturali o modifiche strutturali/breaking.
 """
 
-__version__ = "2.1.5"
+__version__ = "2.1.6"
 APP_VERSION = __version__
 
 
